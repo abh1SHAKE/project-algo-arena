faa50aae6e75457208fc48ba4f057131d658b5cb
Abhishek Sharma <abhshake17@gmail.com>
2025-03-14 23:36:01 +0530

    wip navbar

--- algo-arena/src/app/components/Navbar.tsx
+++ algo-arena/src/app/components/Navbar.tsx
@@ -7,15 +7,19 @@ type NavbarProps = {
 
 const Navbar:React.FC<NavbarProps> = () => {
     return (
-        <div className={`${styles["navbar-container-wrapper"]} bg-black`}>
-            <div className={`${styles["navbar-container"]} flex-row`}>
+        <div className={`${styles["navbar-container-wrapper"]}`}>
+            <div className={`${styles["navbar-container"]} flex-row justify-content-space-between`}>
                 <div className={`${styles["navbar-leftside"]} flex-row align-items-center`}>
-                    <div className={`${styles["logo-container"]}`}>
+                    <div className={`${styles["logo-container"]} text-aquamarine`}>
                         ALGO - ARENA
                     </div>
                 </div>
-                <div className={`${styles["navbar-rightside"]}`}>
-
+                <div className={`${styles["navbar-rightside"]} flex-row align-items-center`}>
+                    <div className={`${styles["cta-container"]}`}>
+                        <button>
+                            Sign in
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>

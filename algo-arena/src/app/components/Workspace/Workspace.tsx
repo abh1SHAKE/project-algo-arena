f644f238f8d9a3a19c86b22daa77b85cac6a4831
Abhishek Sharma <abhshake17@gmail.com>
2025-06-03 23:55:46 +0530

    fixed the split resize gutter

--- algo-arena/src/app/components/Workspace/Workspace.tsx
+++ algo-arena/src/app/components/Workspace/Workspace.tsx
@@ -1,15 +1,30 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Split from 'react-split';
 import styles from './Workspace.module.css';
 import ProblemDetails from './ProblemDetails/ProblemDetails';
 
 const Workspace:React.FC = () => {
+    const [minSize, setMinSize] = useState<[number, number]>([0,0]);
+
+    useEffect(() => {
+        const updateMinSizes = () => {
+            const width = window.innerWidth;
+            const leftMin = width * 0.30;
+            const rightMin = width * 0.50;
+            setMinSize([leftMin, rightMin]);
+        };
+
+        updateMinSizes();
+        window.addEventListener('resize', updateMinSizes);
+        return () => window.removeEventListener('resize', updateMinSizes);
+    },[]);
+
     return (
         <Split 
             className={`${styles["workspace-container"]} flex-row split`}
-            sizes={[40, 60]}
-            minSize={[450, 750]}
+            sizes={[35, 65]}
+            minSize={minSize}
             dragInterval={1}
             snapOffset={0}
         >
